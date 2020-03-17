fde05e4b80a69c1cd81e8d2f05d64073c1a87e13
Nassim18 <nassim_sehout@yahoo.fr>
2020-03-17 14:47:09 +0100

    Changement : classe " Chambre "

--- WebApplication1/Chambre.cs
+++ WebApplication1/Chambre.cs
@@ -5,97 +5,89 @@ namespace WebApplication1
 {
     public class Chambre
     {
-        private int numero, etage, nbr_lits,prix;
-        private Boolean etat;
+        private int numero;
+        private int etage;
         private String type;
-        private List<Reservation> resarvations;
-        public int Numero
-        {
-            get
-            {
-                return numero;
-            }
-
-            set
-            {
-                numero = value;
-            }
-        }
-
-        public int Etage
-        {
-            get
-            {
-                return etage;
-            }
-
-            set
-            {
-                etage = value;
-            }
-        }
+        private int nbr_lits;
+        private int prix;
+        private List<Reservation> reservations;
 
 
-        public int Nbr_lits
-        {
-            get
-            {
-                return nbr_lits;
-            }
 
-            set
-            {
-                nbr_lits = value;
-            }
-        }
-
-        public int Prix
-        {
-            get
-            {
-                return prix;
-            }
-
-            set
-            {
-                prix = value;
-            }
-        }
-
-        public bool Etat
-        {
-            get
-            {
-                return etat;
-            }
-
-            set
-            {
-                etat = value;
-            }
-        }
-
-        public string Type
-        {
-            get
-            {
-                return type;
-            }
-
-            set
-            {
-                type = value;
-            }
-        }
-
-        public Chambre(int numero, int etage, int nbr_lits, int prix, bool etat, string type)
+        public Chambre(int numero, int etage, string type, int nbr_lits, int prix)
         {
             this.Numero = numero;
             this.Etage = etage;
+            this.Type = type;
             this.Nbr_lits = nbr_lits;
             this.Prix = prix;
-            this.Etat = etat;
-            this.Type = type;
+        }
+
+        public Boolean estDisponible(Chambre c, DateTime checkIn, DateTime checkOut)
+        {
+            Boolean res = false;
+            reservations = new List<Reservation>();
+            for (int i = 0; i < reservations.Count; i++)
+            {
+                if (checkIn < reservations[i].CheckIn)
+                {
+                    if (checkIn < reservations[i].CheckOut)
+                    {
+                        if (checkOut < reservations[i].CheckOut)
+                        {
+                            res = true;
+                            return res;
+                        }
+                        else
+                        {
+                            return res;
+                        }
+                    }
+                    else
+                    {
+                        return res;
+                    }
+                }
+                else
+                {
+                    res = true;
+                    return res;
+                }
+
+            }
+            return res;
+
+        }
+
+        public int Numero
+        {
+            get => numero;
+            set => numero = value;
+        }
+        public int Etage
+        {
+            get => etage;
+            set => etage = value;
+        }
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+        public int Nbr_lits
+        {
+            get => nbr_lits;
+            set => nbr_lits = value;
+        }
+        public int Prix
+        {
+            get => prix;
+            set => prix = value;
+        }
+        public List<Reservation> Reservations
+        {
+            get => reservations;
+            set => reservations = value;
         }
     }
 }
